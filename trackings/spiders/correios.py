a450b625ad3a00276e3c002c3489fe0976ca65e3
Paulo Romeira <paulo@pauloromeira.com>
2017-05-04 18:28:03 -0300

    Add correios spider implementation

--- trackings/spiders/correios.py
+++ trackings/spiders/correios.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import FormRequest
 
 
 class CorreiosSpider(scrapy.Spider):
     name = 'correios'
     allowed_domains = ['correios.com.br']
-    start_urls = ['http://correios.com.br/']
+
+    def start_requests(self):
+        url='http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
+        headers={ 'Referer':'http://www.correios.com.br/para-voce' }
+        formdata={ 'objetos': '<tracking-number>' }
+
+        yield FormRequest(url, headers=headers, formdata=formdata)
 
     def parse(self, response):
-        pass
+        from scrapy.shell import open_in_browser
+        open_in_browser(response)
